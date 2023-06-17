dee70d703a74e183087ec73a9aedcc62688240b9
maxefimov <efimov.top.academy@gmail.com>
2023-06-17 17:36:17 +0300

    Добавьте файлы проекта.

---
+++ NetworkUdpSocketApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using Socket socketUdp = new Socket(AddressFamily.InterNetwork, 
+                                    SocketType.Dgram, 
+                                    ProtocolType.Udp);
+var localPoint = new IPEndPoint(IPAddress.Loopback, 7777);
+socketUdp.Bind(localPoint);
+
+Console.WriteLine("UDP server start...");
+
+byte[] buffer  = new byte[1024];
+
+EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+while(true)
+{
+    var result = await socketUdp.ReceiveFromAsync(buffer, remoteIp);
+    var message = Encoding.UTF8.GetString(buffer, 0, result.ReceivedBytes);
+    if (result.ReceivedBytes == 0) break;
+    Console.WriteLine($"Sends {result.ReceivedBytes} bytes from {result.RemoteEndPoint}");
+    Console.WriteLine(message);
+}
+
+
---
+++ NetworkUdpSocketClientApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using Socket socketUdp = new Socket(AddressFamily.InterNetwork,
+                                    SocketType.Dgram,
+                                    ProtocolType.Udp);
+
+string message = "Hello world, from UDP Socket!";
+byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+EndPoint remoteEP = new IPEndPoint(IPAddress.Loopback, 7777);
+int bytes = await socketUdp.SendToAsync(buffer, remoteEP);
+Console.WriteLine($"Sends {bytes} bytes");
